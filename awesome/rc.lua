78cda2e780d3e6f88a64630c735cbc453adeba72
Plamen Stoev <psstoev@gmail.com>
2012-06-24 19:49:43 +0300

    Add autostarted applications

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -343,3 +343,18 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+
+-- {{{ Autostart applications
+function run_once(cmd)
+  findme = cmd
+  firstspace = cmd:find(" ")
+  if firstspace then
+    findme = cmd:sub(0, firstspace-1)
+  end
+  awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
+end
+
+run_once("nm-applet")
+run_once("blueman-applet")
+-- }}}
