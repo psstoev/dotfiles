58a38e92457f57737069740b023bc73b9a74e70f
Plamen Stoev <psstoev@gmail.com>
2013-05-27 10:04:14 +0300

    Zsh, tmux and urxvt configuration

--- zsh/variables.sh
+++ zsh/variables.sh
@@ -1,4 +1,5 @@
 # Custom environment variables:
 
 export EDITOR=vim
-PATH=$HOME/.gem/ruby/1.9.1/bin:$HOME/bin:/usr/bin:$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+export PATH=$HOME/bin:/usr/bin:$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+export HOSTNAME=$HOST
