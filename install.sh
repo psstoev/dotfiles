6ad4b2f167a2904ec101f6985a89ecd7ef0790ce
Plamen Stoev <psstoev@gmail.com>
2013-09-09 13:21:11 +0300

    Use GNU Stow to manage the installation of dotfiles
    http://brandon.invergo.net/news/2012-05-26-using-gnu-stow-to-manage-your-dotfiles.html

--- install.sh
+++ install.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-# A very basic script that makes the appropriate links:
+# Create the symlinks:
+DIRS=`find * -maxdepth 0 -type d`
 
-function create_new_link() {
-	if [[ ! -e $1\/$3 ]]
-	then
-		ln -s `pwd`\/$2 $1\/$3
-	fi
-}
-
-HOME=~
-declare -a COPY_TO_HOME=(gitconfig gvimrc tmux.conf urlview urxvt vim vimrc Xdefaults xxkbrc zsh zshrc)
-declare -a COPY_TO_CONFIG=(awesome)
-
-
-for conffile in ${COPY_TO_HOME[*]}
-do
-	create_new_link $HOME $conffile .$conffile
-done
-
-for conffile in ${COPY_TO_CONFIG[*]}
-do
-	create_new_link $HOME/.config $conffile $conffile
+for config_dir in $DIRS; do
+  stow $config_dir;
 done
