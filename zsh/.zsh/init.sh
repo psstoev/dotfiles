bff840d5752a9879ed3ad190edcca314a0ac3e06
Plamen Stoev <psstoev@gmail.com>
2013-11-18 19:46:12 +0200

    Add custom completion for a program and run editor

--- zsh/.zsh/init.sh
+++ zsh/.zsh/init.sh
@@ -7,6 +7,7 @@ load_defaults
 load_aliases
 #load_correction
 load_completion ${HOME}/.zsh/zshuery/completion
+load_completion ${HOME}/.zsh/completion
 
 # I had to set it again, beacause of some strange bug in Ubuntu
 setopt autocd beep
@@ -16,6 +17,11 @@ source ${HOME}/.zsh/variables.sh
 
 # Customized functions:
 source ${HOME}/.zsh/functions.sh
+
+# Edit long command with $EDITOR:
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '\C-x\C-e' edit-command-line
 
 # Custom functions:
 for i in ${HOME}/.zsh/custom/*; do
